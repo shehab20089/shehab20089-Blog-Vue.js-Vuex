4e57fb29d8c52f6bfe1a4a35346ce7ce17a34765
shehab20089 <xxxegygamerxxx@gmail.com>
2020-01-27 23:24:47 +0200

    adding comment system to the post page

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -20,10 +20,64 @@
       <v-divider></v-divider>
 
       <div class="ma-5">
+        <v-list three-line>
+          <template>
+            <v-subheader class="headline black--text">Comments</v-subheader>
+
+            <v-divider></v-divider>
+
+            <v-list-item v-for="(comment, index) in getPostById(id).comments" :key="index">
+              <v-list-item-avatar size="50" color="grey">
+                <!-- <v-img></v-img> -->
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title class="title">{{comment.author}}</v-list-item-title>
+                <v-list-item-subtitle>{{Date().substring(0,16)}}</v-list-item-subtitle>
+                <p class="body-2">{{comment.commentContent}}</p>
+                <v-list v-if="comment.secondLComments.length>0" three-line>
+                  <template>
+                    <v-subheader>Replies</v-subheader>
+
+                    <v-divider></v-divider>
+
+                    <v-list-item
+                      v-for="(commenLvl2, index) in comment.secondLComments"
+                      :key="index"
+                    >
+                      <v-list-item-avatar size="50" color="grey"></v-list-item-avatar>
+
+                      <v-list-item-content>
+                        <v-list-item-title class="title">{{commenLvl2.author}}</v-list-item-title>
+                        <v-list-item-subtitle>{{Date().substring(0,16)}}</v-list-item-subtitle>
+                        <p class="body-2">{{commenLvl2.commentContent}}</p>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <div class="d-flex align-content-center">
+                      <v-avatar class="mr-6" size="50" color="grey"></v-avatar>
+                      <v-text-field
+                        @keyup.enter="addComment2LvlStore(comment.id,index)"
+                        class="mt-2"
+                        solo
+                        clearable
+                        rounded
+                        :id="'lvl2Comment'+index"
+                        dense
+                        name="comment"
+                        hint="press enter to add  Comment"
+                        label="Add Comment"
+                      ></v-text-field>
+                    </div>
+                  </template>
+                </v-list>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
         <div class="d-flex align-content-center">
           <v-avatar class="mr-6" size="50" color="grey"></v-avatar>
           <v-text-field
-            @keyup.enter="addComment()"
+            @keyup.enter="addCommentStore"
             class="mt-2"
             solo
             ref="CommentRef"
@@ -37,49 +91,6 @@
             id="id"
           ></v-text-field>
         </div>
-        <v-list three-line>
-          <template>
-            <v-subheader>Comments</v-subheader>
-
-            <v-divider inset="1"></v-divider>
-
-            <v-list-item>
-              <v-list-item-avatar size="50" color="grey">
-                <!-- <v-img></v-img> -->
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>this is titile mother</v-list-item-title>
-                <v-list-item-subtitle>this is subtitile</v-list-item-subtitle>
-                <p class="subtitle-2">
-                  adfsafadfdsfdsfsdfdfdsfsdfakshgisuadbfgiudsabgs
-                  <br />adfsafadfdsfdsfsdfdfdsfsdfakshgisuadbfgiudsabgs
-                  adfsafadfdsfdsfsdfdfdsfsdfakshgisuadbfgiudsabgs
-                  adfsafadfdsfdsfsdfdfdsfsdfakshgisuadbfgiudsabgsasdsad
-                  adfsafadfdsfdsfsdfdfdsfsdfakshgisuadbfgiudsabgs
-                </p>
-                <v-list three-line>
-                  <template>
-                    <v-subheader>Replies</v-subheader>
-
-                    <v-divider inset="1"></v-divider>
-
-                    <v-list-item @click="1+1">
-                      <v-list-item-avatar size="50" color="grey">
-                        <!-- <v-img></v-img> -->
-                      </v-list-item-avatar>
-
-                      <v-list-item-content>
-                        <v-list-item-title>this is titile mother</v-list-item-title>
-                        <v-list-item-subtitle>this is subtitile</v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </template>
-                </v-list>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-        </v-list>
       </div>
     </v-card>
   </div>
@@ -90,27 +101,54 @@ import StoreConstants from "../store/storeConstants";
 
 //this helper will allow to easily acess vuex store easily by using the spread operator
 //it will get all getters from the store
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 //just making it easier to acess getters
+let actions = StoreConstants.actions;
 let getters = StoreConstants.getters;
 export default {
   name: "post",
   data() {
     return {
       id: this.$route.params.id,
-      commentText: ""
+      commentText: "",
+      comment2LevelText: ""
     };
   },
   components: {
     // postCard
   },
   methods: {
-    addComment: function() {
+    ...mapActions([actions.addComment, actions.addlvl2Comment]),
+
+    addCommentStore: function() {
       //this comment to avoid eslint console error
       // eslint-disable-next-line no-console
-      console.log(this.commentText);
+
+      // i have hard coded author name to current user of course this should be dynamic but for testing the
+      //features of this task i wil hard code it
+      let newCommentobj = {
+        id: Date.now(),
+        author: "current User",
+        commentContent: this.commentText,
+        secondLComments: []
+      };
+      this.addComment({ postid: this.id, comment: newCommentobj });
       this.commentText = "";
+    },
+    addComment2LvlStore: function(CommentId, index) {
+      let commentLVL2 = document.getElementById("lvl2Comment" + index).value;
+
+      document.getElementById("lvl2Comment" + index).value = "";
+      let newCommentobj = {
+        author: "current User",
+        commentContent: commentLVL2
+      };
+      this.addlvl2Comment({
+        postid: this.id,
+        commentid: CommentId,
+        comment: newCommentobj
+      });
     }
   },
   computed: {
