5e1fccde04737870af108e19653b9f944adccb09
shehab20089 <xxxegygamerxxx@gmail.com>
2020-01-27 08:41:13 +0200

    minor fix

--- src/router/routePaths.js
+++ src/router/routePaths.js
@@ -1,5 +1,5 @@
 //made this class to easily using route names in my views and components
-export default class RoutePathes {
+export default class RoutePaths {
   static Home = "/";
   static Posts = "/Posts";
 }
