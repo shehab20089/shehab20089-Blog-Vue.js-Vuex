1ecbcd91c57687d554c5a4294565fe38b280eaef
shehab20089 <xxxegygamerxxx@gmail.com>
2020-01-27 11:25:20 +0200

    adding store constans to make it easir to use it

---
+++ src/store/storeConstants.js
@@ -0,0 +1,7 @@
+//made this class to easily using route names in my views and components
+export default class StoreConstants {
+  static getters = {
+    getPosts: "getPosts",
+    getPostById: "getPostById"
+  };
+}
